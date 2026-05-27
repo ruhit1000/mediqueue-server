bea9b83d11478b874fa248f4f22aed6af8b21b1e
Ruhit Paul <ruhitpaul18@gmail.com>
2026-05-27 18:00:15 +0600

    Set up MongoDB connection and update server configuration

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config();
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
+const run = async () => {
+    try {
+        await client.connect();
+        // await client.db("admin").command({ ping: 1 });
+        
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -8,4 +33,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});v
+});
